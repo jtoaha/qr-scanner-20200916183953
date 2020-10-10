1a7c70b91faa75a6f16b11f118565186974b2bb1
Jamila Toaha <jtoahacoding@gmail.com>
2020-10-10 02:01:56 -0400

    (feat) added file upload fallback

--- src/components/QRScanner.tsx
+++ src/components/QRScanner.tsx
@@ -1,4 +1,4 @@
-import React, {createRef} from 'react';
+import React, { createRef } from 'react';
 import { IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/react';
 import './QRScanner.css';
 import jsQR from 'jsqr';
@@ -15,18 +15,18 @@ interface ContainerState {
 }
 
 class QRScanner extends React.Component<ContainerProps, ContainerState> {
-private video = createRef<HTMLVideoElement>();
-private canvas = createRef<HTMLCanvasElement>();
-private fileInput = createRef<HTMLInputElement>();
-// loading: HTMLIonLoadingElement = null;
+  private video = createRef<HTMLVideoElement>();
+  private canvas = createRef<HTMLCanvasElement>();
+  private fileInput: any;
+  // loading: HTMLIonLoadingElement = null;
 
-private canvasElement: any;
-private videoElement: any;
-private canvasContext: any;
-private stream: any;
-// private loading!: HTMLIonLoadingElement;
+  private canvasElement: any;
+  private canvasContext: any;
+  private videoElement: any;
+  private stream: any;
+  // private loading!: HTMLIonLoadingElement;
 
-  constructor(props: ContainerProps){
+  constructor(props: ContainerProps) {
     super(props);
     this.state = {
       scanActive: false,
@@ -39,23 +39,25 @@ private stream: any;
     this.scan = this.scan.bind(this);
     this.stopScan = this.stopScan.bind(this)
     this.refresh = this.refresh.bind(this)
-
+    this.uploadImage = this.uploadImage.bind(this)
+    this.handleFile = this.handleFile.bind(this)
   }
 
-  componentDidMount(){
+  componentDidMount() {
     //  this.canvasElement = this.canvas.current!
     //  this.videoElement = this.video.current!
     //  this.canvasContext = this.canvasElement.getContext('2d')
-    }
+  }
 
-  async componentDidUpdate(){
+  async componentDidUpdate() {
     this.canvasElement = this.canvas.current!
     this.videoElement = this.video.current!
     this.canvasContext = this.canvasElement.getContext('2d')
+    this.fileInput = document.getElementById('file-input')
   }
 
 
-  videoError (){
+  videoError() {
 
   }
 
@@ -63,20 +65,17 @@ private stream: any;
 
   }
 
-  captureImage(){
-
-  }
 
   /*
    startScan sets up the stream and then sets up a continuous call to scan via requestAnimationFrame
   */
-  async startScan(){
-  // Not working on iOS standalone mode!
+  async startScan() {
+    // Not working on iOS standalone mode!
     this.stream = await navigator.mediaDevices.getUserMedia({
       video: { facingMode: 'environment' }
     });
 
-    this.setState({scanActive: true});
+    this.setState({ scanActive: true });
 
     this.videoElement.srcObject = this.stream;
     // Required for Safari
@@ -121,7 +120,8 @@ private stream: any;
       if (code) {
         this.setState({
           scanActive: false,
-          scanResult: code.data});
+          scanResult: code.data
+        });
         //this.showQrToast();
       } else {
         if (this.state.scanActive) {
@@ -136,60 +136,90 @@ private stream: any;
     }
   }
 
-  /* stops stream */
-  stopScan(){
-    this.stream.getTracks()[0].stop();
+  /**
+   * stops scan when user presses corresponding button.
+   * - stops stream and sets the state of scanActive to false
+   */
+  stopScan() {
     this.videoElement.setAttribute('playsinline', false);
-    this.setState({scanActive: false});
+    this.stream.getTracks()[0].stop();
+    this.setState({ scanActive: false });
   }
 
-  refresh(){
-    this.setState({scanResult: '', scanActive: false});
+  refresh() {
+    this.stream.getTracks()[0].stop();
+    this.setState({ scanResult: '', scanActive: false });
   }
 
-  handleFile(event: React.ChangeEvent<HTMLElement>): void{
-    //e.target.value
+
+  handleFile(event: React.ChangeEvent<HTMLElement>): void {
+    const file = this.fileInput.files.item(0);
+
+    var img = new Image();
+    img.onload = () => {
+      this.canvasContext.drawImage(img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+
+      if (code) {
+        this.setState({ scanResult: code.data });
+        // this.showQrToast();
+      }
+    };
+    img.src = URL.createObjectURL(file);
   }
 
-  render(){
+  uploadImage() {
+    this.fileInput.click();
+  }
+
+
+  render() {
     console.log(this.videoElement, "jooo")
-  return (
-    <div className="container">
-      <strong>{this.props.name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      <p> Hello World!</p>
+    return (
+      <div className="container">
+        <strong>{this.props.name}</strong>
+        <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <p> Hello World!</p>
 
-      {/* -- Fallback for iOS PWA -- */}
-      <input type="file" accept="image/*;capture=camera" hidden onChange={this.handleFile} ref={this.fileInput}/>
+        {/* -- Fallback for iOS PWA -- */}
+        <input id="file-input" type="file" accept="image/*;capture=camera" hidden onChange={this.handleFile} ref={this.fileInput} />
 
 
-      {/* --Trigger the file input-- */}
-      <IonButton id="camera" shape="round" onClick={this.captureImage} color="primary">Capture Image</IonButton>
-      <IonButton id="qr-scanner" shape="round" onClick={this.startScan} color="primary">Start Scan</IonButton>
-      <IonButton id="refresh" shape="round" onClick={this.refresh} color="warning">Reset</IonButton>
+        {/* --Trigger the file input-- */}
+        <IonButton id="camera" shape="round" onClick={this.uploadImage} color="primary">Upload Image</IonButton>  - or -
+        <IonButton id="qr-scanner" shape="round" onClick={this.startScan} color="primary">Start Scan</IonButton> <br />
+        <IonButton id="refresh" shape="round" onClick={this.refresh} color="warning">Reset</IonButton>
 
-      {/* --Shows our camera stream-- */}
-      {/* <video width="50%" ref={this.video} autoPlay={true}/> */}
+        {/* --Shows our camera stream-- */}
+        {/* <video width="50%" ref={this.video} autoPlay={true}/> */}
 
-      <video hidden={!this.state.scanActive} width="100%" ref={this.video} autoPlay={true}/>
-      {/* <Video videoRef={this.video} srcObject={new MediaStream()}/> */}
+        <video hidden={!this.state.scanActive} width="100%" ref={this.video} autoPlay={true} />
+        {/* <Video videoRef={this.video} srcObject={new MediaStream()}/> */}
 
-      {/* --Used to render the camera stream images-- */}
-      <canvas hidden ref={this.canvas}></canvas>
+        {/* --Used to render the camera stream images-- */}
+        <canvas hidden ref={this.canvas}></canvas>
 
-      {/* --Stop our scanner preview if active-- */}
-      {this.state.scanActive ? <IonButton shape="round" onClick={this.stopScan} color="danger">Stop Scan</IonButton>: null}
+        {/* --Stop our scanner preview if active-- */}
+        {this.state.scanActive ? <IonButton shape="round" onClick={this.stopScan} color="danger">Stop Scan</IonButton> : null}
 
-      {/* --Display scanner result-- */}
-      <IonCard>
-        <IonCardHeader>
-        <IonCardTitle>QR Scan Result:</IonCardTitle>
-        </IonCardHeader>
-        <IonCardContent>{this.state.scanResult}</IonCardContent>
-     </IonCard>
-    </div>
+        {/* --Display scanner result-- */}
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>QR Scan Result:</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>{this.state.scanResult}</IonCardContent>
+        </IonCard>
+      </div>
 
-  );
+    );
   }
 }
 
